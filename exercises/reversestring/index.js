c986c28d7964aef67978f30b4541cd85e4630c5e
shambho <sivakrishna.karasala@eku.edu>
2019-03-14 08:03:09 -0400

    add reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
 
 module.exports = reverse;
+
+// Sol 1
+
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+// Sol 2
+
+// function reverse(str) {
+//   let reversed = "";
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+// Sol 3
+
+// function reverse(str) {
+//   debugger;
+//   return str.split("").reduce((reversed, char) => char + reversed, "");
+// }
+
+// reverse("shiva");
