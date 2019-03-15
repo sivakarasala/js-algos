272cd862e2e72d886bb4101f1d1cfc6f18a14d9c
shambho <sivakrishna.karasala@eku.edu>
2019-03-15 09:43:17 -0400

    reverse integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  return (
+    parseInt(
+      Math.abs(n)
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(n)
+  );
+}
 
 module.exports = reverseInt;
+
+// Alternate Sol
+
+// function reverseInt(n) {
+//   const str = Math.abs(n)
+//     .toString()
+//     .split("");
+//   let reversed = "";
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return parseInt(reversed) * Math.sign(n);
+// }
