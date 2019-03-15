44f08450134f55a418fa0ef6f2d35fd12177b640
shambho <sivakrishna.karasala@eku.edu>
2019-03-15 09:58:25 -0400

    find maxchar in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0;
+  let maxChar = "";
+  const charMap = {};
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  for (char in charMap) {
+    if (charMap[char] > max) {
+      maxChar = char;
+      max = charMap[char];
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
